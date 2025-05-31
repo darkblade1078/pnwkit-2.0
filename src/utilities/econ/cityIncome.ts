f7fca497f005fe80d1856eeb5fd95f8f2a330c89
S0l4RIS <silverphantom15@gmail.com>
2025-05-31 18:00:49 +1000

    Added BDA effect on Dom Policy to Utils functions

--- src/utilities/econ/cityIncome.ts
+++ src/utilities/econ/cityIncome.ts
@@ -6,10 +6,11 @@
  * @param {number} population How many people live in your city
  * @param {boolean} OP If you are running open markets
  * @param {boolean} GSA If you have Government Support Agency
+ * @param {boolean} BDA If you have Bureau of Domestic Affairs
  * @returns {number} How much money a you produce in a day
  */
-export default function cityIncome(cities: number, commerce: number, population: number, OP: boolean, GSA: boolean): number {
-    
-    let multipler = (OP && GSA) ? 1.015 : (OP ? 1.01 : 1);
+export default function cityIncome(cities: number, commerce: number, population: number, OP: boolean, GSA: boolean, BDA: boolean): number {
+
+    let multipler = OP ? BDA ? 1.0175 : GSA ? 1.015 : 1.01 : 0;
     return (((((commerce / 50) * 0.725) + 0.725) * population) * multipler) * cities;
 }
