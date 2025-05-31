9d387cc891ba9a6af9479ed9e156835575da578b
S0l4RIS <silverphantom15@gmail.com>
2025-05-31 17:30:40 +1000

    Added research cost and accounted for research in foodConsumption

--- src/utilities/econ/foodConsumption.ts
+++ src/utilities/econ/foodConsumption.ts
@@ -2,13 +2,16 @@ import { city } from "../../interfaces/queries/city";
 import getDaysBetween from "../other/getDaysBetween";
 
 /**
- * Gets the amount of food consumed in a day
- * @param {number} basePopulation The base population of your cities
- * @param {number} cityAge The age of your oldest city
- * @param {number} cities How many cities you have
- * @returns {number} How much food you consume in a day
+ * Gets how much food a nation consumes in a day
+ *
+ * @export
+ * @param {city[]} cities Array of nations cities including infrastructure and date
+ * @param {number} soldiers The nations soldiers
+ * @param {?boolean} [atWar] Whether the nation is at war or not
+ * @param {number} [groundCost=0] The number of groundCost research levels the nation has
+ * @returns {number} 
  */
-export default function foodConsumption(cities: city[], soldiers: number, atWar?: boolean): number {
+export default function foodConsumption(cities: city[], soldiers: number, atWar?: boolean, groundCost: number = 0): number {
     if (!cities[0].infrastructure) throw new Error(`PnwKit: missing cities.infrastructure`);
     if (!cities[0].date) throw new Error(`PnwKit: missing cities.date`);
 
@@ -16,12 +19,12 @@ export default function foodConsumption(cities: city[], soldiers: number, atWar?
 
     let cost = 0;
 
-    for(let i = 0; i < cities.length; i++) {
+    for (let i = 0; i < cities.length; i++) {
         let city = cities[i];
         let oldDate = new Date(city.date);
 
-        cost +=  (((city.infrastructure! * 100) ** 2) / 125_000_000) + (((city.infrastructure! * 100) * (1 + Math.max(Math.log(getDaysBetween(oldDate, newDate)) / 15, 0)) - city.infrastructure! * 100) / 850)
+        cost += (((city.infrastructure! * 100) ** 2) / 125_000_000) + (((city.infrastructure! * 100) * (1 + Math.max(Math.log(getDaysBetween(oldDate, newDate)) / 15, 0)) - city.infrastructure! * 100) / 850)
     }
 
-    return cost + (atWar ? soldiers / 500 : soldiers / 750);
+    return cost + (atWar ? soldiers / (500 + 30 * groundCost) : soldiers / (750 + 20 * groundCost));
 }
